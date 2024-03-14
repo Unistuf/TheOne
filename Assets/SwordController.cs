fed316b2a3c1bc312338e930c973588863dd11f8
AnonymousMouse3 <evansjunk123@gmail.com>
2024-03-14 20:12:37 +0000

    SwordController preliminary

--- Assets/SwordController.cs
+++ Assets/SwordController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,9 +10,15 @@ public class SwordController : MonoBehaviour
     public PlayerControls controls;
 
     [SerializeField] float attackCooldown = 0.5f;
-    public bool isAttacking;
+    public bool insideZone;
 
-    Vector2 rightStickLocation;
+    string comboString;
+    bool extendingString;
+
+    Vector2 rStickPos;
+    [SerializeField] float zoneWidth = 0.3f;
+    [SerializeField] float zoneHeight = 0.8f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,20 +32,59 @@ public class SwordController : MonoBehaviour
         //Debug.Log("Right: " + rightStickLocation);
     }
 
-    public IEnumerator OnSwordSwing(InputValue value)
+    public void OnSwordSwing(InputValue value)
     {
-        rightStickLocation = value.Get<Vector2>();
+        rStickPos = value.Get<Vector2>();
+        Debug.Log("Input called");
 
-        if (rightStickLocation.y >= 0.8f && rightStickLocation.x <= 0.3f && isAttacking == false)
+
+        if (insideZone == false)
         {
-            isAttacking = true;
+            if (rStickPos.y >= zoneHeight && rStickPos.x <= zoneWidth && rStickPos.x >= -zoneWidth && insideZone == false)
+            {
+                Debug.Log("A");
+                insideZone = true;
 
-            Debug.Log("Thrust");
-
-            yield return new WaitForSeconds(attackCooldown);
-            isAttacking = false;
+                StartCoroutine(SwordAttacks("A"));
+            }
+            else if (rStickPos.y <= zoneWidth && rStickPos.y >= -zoneWidth && rStickPos.x >= zoneHeight && insideZone == false)
+            {
+                Debug.Log("B");
+                insideZone = true;
+            }
+            else if (rStickPos.y <= -zoneHeight && rStickPos.x <= zoneWidth && rStickPos.x >= -zoneWidth && insideZone == false)
+            {
+                Debug.Log("C");
+                insideZone = true;
+            }
+            else if (rStickPos.y <= zoneWidth && rStickPos.y >= -zoneWidth && rStickPos.x <= -zoneHeight && insideZone == false)
+            {
+                Debug.Log("D");
+                insideZone = true;
+            }
+            else
+            {
+                insideZone = false;
+                Debug.Log("false");
+            }
         }
+    }
+
+    public IEnumerator SwordAttacks(string inZone)
+    {
+        if (insideZone == false)
+        {
+            extendingString = true;
+
+            comboString += inZone;
+
+            extendingString = false;
+        }
+
+        Debug.Log(comboString);
 
         yield return null;
     }
+
+
 }

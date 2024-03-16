0142ab6fe662e0c8c5129a7d708c2c2485f8b901
AnonymousMouse3 <evansjunk123@gmail.com>
2024-03-16 19:23:40 +0000

    Finalised SwordController logic, implementing attacks next

--- Assets/SwordController.cs
+++ Assets/SwordController.cs
@@ -10,35 +10,36 @@ public class SwordController : MonoBehaviour
     public PlayerControls controls;
 
     [SerializeField] float attackCooldown = 0.5f;
-    public bool insideZone;
-    public string currentZone;
-
-    string comboString;
 
     Vector2 rStickPos;
     [SerializeField] float zoneWidth = 0.3f;
     [SerializeField] float zoneHeight = 0.8f;
+    string currentZone;
+    bool insideZone;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log("Right: " + rightStickLocation);
-    }
+    // The list of valid attacks and combos that we should check against
+    // To add more moves, increase the first value of comboList and add the new move's name and corresponding combo in the format below
+    string[,] comboList = new string[8, 2]
+        {
+    {"basicThrust", "A"},
+    {"basicRightSwing", "B"},
+    {"basicParry", "C"},
+    {"basicLeftSwing", "D"},
+    {"doubleThrust", "AA" },
+    {"secondRightSwing", "BB" },
+    {"doubleParry", "CC"},
+    {"secondLeftSwing", "DD" },
+};
+    [SerializeField] string comboString = string.Empty;
 
     public void OnSwordSwing(InputValue value)
     {
+        // Collect the input value of our right stick
         rStickPos = value.Get<Vector2>();
-        Debug.Log("Input called");
 
-
-
+        // And then check if it is inside the bounds of any of these "zones" at the top, right, bottom, and left edges of our analogue stick
+        // Each zone is labeled ABCD respectively, and N is the "neutral" zone representing the centre of the stick
+        // The exact bounds of these boxes can be tuned with zoneHeight and zoneWidth
         if (rStickPos.y >= zoneHeight && rStickPos.x <= zoneWidth && rStickPos.x >= -zoneWidth)
         {
             StartCoroutine(SwordAttacks("A"));
@@ -63,23 +64,57 @@ public class SwordController : MonoBehaviour
 
     public IEnumerator SwordAttacks(string inZone)
     {
+        
         if (insideZone == false)
         {
+            // If our stick has entered a new zone, lock this function until we leave it
             insideZone = true;
             currentZone = inZone;
 
+            // And append the zone's ID to the combo string, unless we are in the null zone in the centre
             if (inZone != "N")
             {
                 comboString += inZone;
+                bool foundOnFirstCheck = false;
+
+                // Then, check current the combo against the combo list, and perform any attack or combo that we "hit" at each stage
+                // E.g. if we perform a combo AAC, this means we will perform 3 attacks, A, AA, and AAC
+                for (int i = 0; i < comboList.GetLength(0); i++)
+                {
+                    if (comboString == comboList[i, 1])
+                    {
+                        foundOnFirstCheck = true;
+                        Debug.Log(comboList[i, 0]);
+                    }
+                }
+
+                // And if we do not hit a valid combo, just perform the attack of the most recent letter (e.g. if DDC is not a valid combo, just execute C)
+                if (!foundOnFirstCheck)
+                {
+                    string lastAttack = Char.ToString(comboString[comboString.Length - 1]);
+
+                    for (int i = 0; i < comboList.GetLength(0); i++)
+                    {
+                        if (lastAttack == comboList[i, 1])
+                        {
+                            Debug.Log(comboList[i, 0]);
+                        }
+                    }
+                }
+            }
+
+            // Cap our combo length to 2
+            if (comboString.Length >= 2)
+            {
+                comboString = string.Empty;
             }
         }
 
+        // If the stick is not inside a zone, allow it to select a new zone
         if (inZone != currentZone)
         {
             insideZone = false;
         }
-
-        Debug.Log(comboString);
 
         yield return null;
     }

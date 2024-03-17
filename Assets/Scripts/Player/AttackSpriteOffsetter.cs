8ec1580af780b8b9e22bf273f399f76c184527f2
AnonymousMouse3 <evansjunk123@gmail.com>
2024-03-17 17:55:23 +0000

    setup for attacks + PLAYER HAS OWN LAYER NOW

--- Assets/Scripts/Player/AttackSpriteOffsetter.cs
+++ Assets/Scripts/Player/AttackSpriteOffsetter.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAttackController : MonoBehaviour
+public class AttackSpriteOffsetter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = transform.parent.transform.position;
     }
 }
